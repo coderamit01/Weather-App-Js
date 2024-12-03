ce8eb239a5bdb4be83ea5ee74ce410fda18867dc
amit <amitmohammad46@gmail.com>
2024-12-04 00:25:38 +0600

    design complete

--- main.js
+++ main.js
@@ -1,16 +1,18 @@
 const apiKey = `5341f3b4f67c822c816bbab17dc83348`;
-const apiUrl = 'https://api.openweathermap.org/data/2.5/weather?q=dhaka&appid=5341f3b4f67c822c816bbab17dc83348&units=metric';
+const apiUrl = 'https://api.openweathermap.org/data/2.5/weather?q=';
 
 const searchBtn = document.getElementById('search-btn');
+const searchBox = document.querySelector('#searchbox');
+// Search city name and get weather data
 searchBtn.addEventListener('click', (e) => {
   e.preventDefault();
-  getWeatherDetails();
+  getWeatherDetails(searchBox.value);
 });
-
-const searchBox = document.querySelector('#searchbox')
-
-const getWeatherDetails = async () => {
-  const fetchData = await fetch(apiUrl);
+// Get WeatherDetails
+const getWeatherDetails = async (city) => {
+  const fetchData = await fetch(`${apiUrl} ${city} &appid=${apiKey}&units=metric`);
   const res = await fetchData.json();
+  const temp = document.querySelector('.temp');
+  const cityName = document.querySelector('.city');
   console.log(res);
 }
