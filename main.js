08546af482fa74dcfe2f2e51c96a7215c9f7cf10
amit <amitmohammad46@gmail.com>
2024-12-04 00:42:38 +0600

    geting data from api when search

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const apiKey = `5341f3b4f67c822c816bbab17dc83348`;
-const apiUrl = 'https://api.openweathermap.org/data/2.5/weather?q=';
+const apiUrl = 'https://api.openweathermap.org/data/2.5/weather?units=metric&q=';
 
 const searchBtn = document.getElementById('search-btn');
 const searchBox = document.querySelector('#searchbox');
@@ -10,9 +10,31 @@ searchBtn.addEventListener('click', (e) => {
 });
 // Get WeatherDetails
 const getWeatherDetails = async (city) => {
-  const fetchData = await fetch(`${apiUrl} ${city} &appid=${apiKey}&units=metric`);
+  const fetchData = await fetch(`${apiUrl} ${city} &appid=${apiKey}`);
   const res = await fetchData.json();
   const temp = document.querySelector('.temp');
   const cityName = document.querySelector('.city');
+  const humidity = document.querySelector('.hm'); 
+  const wind = document.querySelector('.wn'); 
+
+  temp.innerHTML = Math.round(res.main.temp) + ' °C';
+  cityName.innerHTML = res.name;
+  humidity.innerHTML = res.main.humidity + ' %';
+  wind.innerHTML = res.wind.speed + ' km';
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
   console.log(res);
 }
