550f21019fa01cbd59fc4eeb80040b7b9bc076f0
amit <amitmohammad46@gmail.com>
2024-12-04 22:19:52 +0600

    add search functionality

--- main.js
+++ main.js
@@ -11,30 +11,44 @@ searchBtn.addEventListener('click', (e) => {
 // Get WeatherDetails
 const getWeatherDetails = async (city) => {
   const fetchData = await fetch(`${apiUrl} ${city} &appid=${apiKey}`);
-  const res = await fetchData.json();
-  const temp = document.querySelector('.temp');
-  const cityName = document.querySelector('.city');
-  const humidity = document.querySelector('.hm'); 
-  const wind = document.querySelector('.wn'); 
+  if (!fetchData.ok) {
+    document.querySelector('.error').style.display = 'block';
+    document.querySelector('.weather').style.display = 'none';
+  } else {
+    const res = await fetchData.json();
+    const temp = document.querySelector('.temp');
+    const cityName = document.querySelector('.city');
+    const humidity = document.querySelector('.hm');
+    const wind = document.querySelector('.wn');
+    const weatherImg = document.querySelector('.weather-img');
 
-  temp.innerHTML = Math.round(res.main.temp) + ' °C';
-  cityName.innerHTML = res.name;
-  humidity.innerHTML = res.main.humidity + ' %';
-  wind.innerHTML = res.wind.speed + ' km';
+    temp.innerHTML = Math.round(res.main.temp) + ' °C';
+    cityName.innerHTML = res.name;
+    humidity.innerHTML = res.main.humidity + ' %';
+    wind.innerHTML = res.wind.speed + ' km';
+
+    if(res.weather[0].main == "Haze") {
+      weatherImg.src = '/img/clouds.png';
+    }
+    else if(res.weather[0].main == "Rain") {
+      weatherImg.src = '/img/rain.png';
+    }
+    else if(res.weather[0].main == "Clouds") {
+      weatherImg.src = '/img/clouds.png';
+    }
+    else if(res.weather[0].main == "Drizzle") {
+      weatherImg.src = '/img/drizzle.png';
+    }
+    else if(res.weather[0].main == "Mist") {
+      weatherImg.src = '/img/mist.png';
+    }
+    else if(res.weather[0].main == "Clear") {
+      weatherImg.src = '/img/clear.png';
+    }
 
 
+    document.querySelector('.weather').style.display = 'block';
+    document.querySelector('.error').style.display = 'none';
+  }
 
-
-
-
-
-
-
-
-
-
-
-
-
-  console.log(res);
 }
